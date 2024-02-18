b9e2d0e1128a8237b8775af32f654dc10e3f6b75
francisco <frnncs@gmail.com>
2024-02-18 20:53:58 -0300

    hotfix: update astro site in defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  site: "https://frnn.dev",
+  site: "https://frnncs.github.io/portofolio/",
+  base: '/',
 });
