a5906bf74cebf531427c48d74a1e8edbd77a6569
francisco <frnncs@gmail.com>
2024-02-18 21:36:16 -0300

    enhance: agrego pwa functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
+import AstroPWA from '@vite-pwa/astro'
+
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [
+    svelte(),
+    tailwind(),
+    AstroPWA(),
+  ],
   site: "https://frnncs.github.io/portofolio/",
   base: '/',
 });
