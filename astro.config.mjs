ff43a200a757d3eed53633ecb20978b45adb514d
francisco <frnncs@gmail.com>
2024-02-18 21:22:46 -0300

    hotfix: update astro base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   site: "https://frnncs.github.io/portofolio/",
-  base: '/portofolio',
+  base: '/',
 });
