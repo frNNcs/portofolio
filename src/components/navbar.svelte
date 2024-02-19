60346cb6d89897e30ec70ff9e792d76c5b12572d
francisco <frnncs@gmail.com>
2024-02-19 11:06:41 -0300

    enhance: fix blink animation, change catppuccin color prefix, rearange components, adds nn.svg icon

---
+++ src/components/navbar.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar
+	class="
+	bg-latte-base text-latte-text
+	dark:bg-mocha-base dark:text-mocha-text"
+>
+	<NavBrand href="/">
+		<span class="text-xl">
+			<span class="px-0 mx-0 text-wrap">fr</span>
+			<img src="/nn.svg" alt="nn logo" class="w-7 h-7 inline-block px-0 mx-0" />
+			.
+			<span class="font-thin">dev</span>
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi class="cursor-pointer" href="{null}">About</NavLi>
+		<NavLi class="cursor-pointer" href={null}>Blog</NavLi>
+		<NavLi class="cursor-pointer" href={null}>Contact</NavLi>
+	</NavUl>
+</Navbar>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { colors } from '@catppuccin/palette'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -7,13 +9,13 @@ export default {
 
 	plugins: [
 		require('flowbite/plugin'),
-		require("@catppuccin/tailwindcss")({
-			prefix: "ctp",
-			defaultFlavour: "mocha",
-		  }),
+		require('@catppuccin/tailwindcss')({
+			prefix: 'mocha',
+			defaultFlavour: 'mocha'
+		})
 	],
 
-	darkMode: 'class',
+	darkMode: 'media',
 
 	theme: {
 		extend: {
@@ -21,12 +23,19 @@ export default {
 				blinkingText: {
 					'0%': { opacity: 1 },
 					'50%': { opacity: 0 },
-					'100%': { opacity: 1 },
+					'100%': { opacity: 1 }
 				}
 			},
 			animation: {
-				blinkingText: 'blinkingText 1s infinite',
+				blinkingText: 'blinkingText 1s infinite'
 			}
 		},
-	},
+		colors: {
+			...colors,
+			latte: {
+				base: '#eff1f5',
+				text: '#4c4f69'
+			}
+		}
+	}
 }
