d190e93f532afed434c060b4db239098d7099129
francisco <frnncs@gmail.com>
2024-02-19 11:24:23 -0300

    update: add medium url to blog

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -19,7 +19,7 @@
 	<NavUl>
 		<NavLi href="/" active={true}>Home</NavLi>
 		<NavLi class="cursor-pointer" href="{null}">About</NavLi>
-		<NavLi class="cursor-pointer" href={null}>Blog</NavLi>
+		<NavLi class="cursor-pointer" href="https://medium.com/@frnn">Blog</NavLi>
 		<NavLi class="cursor-pointer" href={null}>Contact</NavLi>
 	</NavUl>
 </Navbar>
